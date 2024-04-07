9d5abca81155f5321fa976a4c294a15f36f733c5
Riley Prince <NuggoDEV@gmail.com>
2024-04-07 13:01:55 +0930

    update to 1.35 with new libs added

--- template/include/main.hpp
+++ template/include/main.hpp
@@ -1,15 +1,10 @@
 #pragma once
 
-// Include the modloader header, which allows us to tell the modloader which mod this is, and the version etc.
-#include "modloader/shared/modloader.hpp"
-
-// beatsaber-hook is a modding framework that lets us call functions and fetch field values from in the game
-// It also allows creating objects, configuration, and importantly, hooking methods to modify their values
-#include "beatsaber-hook/shared/utils/logging.hpp"
-#include "beatsaber-hook/shared/config/config-utils.hpp"
+#include "scotland2/shared/loader.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
-#include "beatsaber-hook/shared/utils/hooking.hpp"
+#include "paper/shared/logger.hpp"
 
-// Define these functions here so that we can easily read configuration and log information from other files
-Configuration& getConfig();
-Logger& getLogger();
+constexpr auto logger = Paper::ConstLoggerContext(MOD_ID);
+
+#define MOD_EXPORT __attribute__((visibility("default")))
+#define MOD_EXPORT_FUNC extern "C" MOD_EXPORT
--- template/src/main.cpp
+++ template/src/main.cpp
@@ -1,35 +1,17 @@
 #include "main.hpp"
+#include "hooks.hpp"
 
-static ModInfo modInfo; // Stores the ID and version of our mod, and is sent to the modloader upon startup
+static modloader::ModInfo modInfo{ MOD_ID, VERSION, 0 };
 
-// Loads the config from disk using our modInfo, then returns it for use
-// other config tools such as config-utils don't use this config, so it can be removed if those are in use
-Configuration& getConfig() {
-    static Configuration config(modInfo);
-    return config;
+
+MOD_EXPORT_FUNC void setup(CModInfo& info) {
+    info = modInfo.to_c();
+	logger.info("Completed Setup!");
 }
 
-// Returns a logger, useful for printing debug messages
-Logger& getLogger() {
-    static Logger* logger = new Logger(modInfo);
-    return *logger;
-}
-
-// Called at the early stages of game loading
-extern "C" void setup(ModInfo& info) {
-    info.id = MOD_ID;
-    info.version = VERSION;
-    modInfo = info;
-	
-    getConfig().Load();
-    getLogger().info("Completed setup!");
-}
-
-// Called later on in the game loading - a good time to install function hooks
-extern "C" void load() {
+MOD_EXPORT_FUNC void late_load() {
     il2cpp_functions::Init();
 
-    getLogger().info("Installing hooks...");
-    // Install our hooks (none defined yet)
-    getLogger().info("Installed all hooks!");
+    ${mod_name}::Hooks::InstallHooks();
+    logger.info("Completed Load");
 }
